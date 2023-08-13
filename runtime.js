6a6da6788981a5bf2321e564e359f1b26464302e
Andreas Alin <andreas.alin@gmail.com>
2023-08-13 13:03:12 -0500

    Restructure and add copyright

--- runtime.js
+++ runtime.js
@@ -1,64 +1,5 @@
-const Patches = {
-  CreateElement(id, type) {
-    this.setNode(id, document.createElement(type));
-  },
-  CreateTextNode(id, content) {
-    this.setNode(id, document.createTextNode(content));
-  },
-  CreateComment(id, content) {
-    this.setNode(id, document.createComment(content));
-  },
-  RemoveNode(id) {
-    this.deleteNode(id);
-  },
-  SetAttribute(id, name, value) {
-    this.getNode(id).setAttribute(name, value);
-  },
-  RemoveAttribute(id, name) {
-    this.getNode(id).removeAttribute(name);
-  },
-  SetCSSProperty(id, name, value) {
-    console.debug("SetCSSProperty", id, name, value);
-    this.getNode(id).style.setProperty(name, value);
-  },
-  RemoveCSSProperty(id, name) {
-    this.getNode(id).style.removeProperty(name);
-  },
-  SetTextContent(id, content) {
-    this.getNode(id).data = content;
-  },
-  ReplaceChildren(id, childIds) {
-    this.getNode(id).replaceChildren(...this.getNodes(childIds));
-  },
-};
-
-class NodeSet {
-  #nodes = {};
-
-  deleteNode(id) {
-    console.debug("deleteNode", id)
-    delete this.#nodes[id];
-  }
-
-  setNode(id, node) {
-    console.debug("setNode", id)
-    this.#nodes[id] = node;
-  }
-
-  getNode(id) {
-    const node = this.#nodes[id];
-
-    if (!node) {
-      throw new Error(`Node not found: ${id}`);
-    }
-
-    return node;
-  }
-
-  getNodes(ids) {
-    return ids.map((id) => this.getNode(id));
-  }
-}
+// Copyright Andreas Alin <andreas.alin@gmail.com>
+// License: AGPL-3.0
 
 export default class Runtime {
   #nodeSet = new NodeSet();
@@ -81,3 +22,62 @@ export default class Runtime {
     Patches[name].apply(this.#nodeSet, args);
   }
 }
+
+class NodeSet {
+  #nodes = {};
+
+  deleteNode(id) {
+    delete this.#nodes[id];
+  }
+
+  setNode(id, node) {
+    this.#nodes[id] = node;
+  }
+
+  getNode(id) {
+    const node = this.#nodes[id];
+
+    if (!node) {
+      throw new Error(`Node not found: ${id}`);
+    }
+
+    return node;
+  }
+
+  getNodes(ids) {
+    return ids.map((id) => this.getNode(id));
+  }
+}
+
+const Patches = {
+  CreateElement(id, type) {
+    this.setNode(id, document.createElement(type));
+  },
+  CreateTextNode(id, content) {
+    this.setNode(id, document.createTextNode(content));
+  },
+  CreateComment(id, content) {
+    this.setNode(id, document.createComment(content));
+  },
+  RemoveNode(id) {
+    this.deleteNode(id);
+  },
+  SetAttribute(id, name, value) {
+    this.getNode(id).setAttribute(name, value);
+  },
+  RemoveAttribute(id, name) {
+    this.getNode(id).removeAttribute(name);
+  },
+  SetCSSProperty(id, name, value) {
+    this.getNode(id).style.setProperty(name, value);
+  },
+  RemoveCSSProperty(id, name) {
+    this.getNode(id).style.removeProperty(name);
+  },
+  SetTextContent(id, content) {
+    this.getNode(id).data = content;
+  },
+  ReplaceChildren(id, childIds) {
+    this.getNode(id).replaceChildren(...this.getNodes(childIds));
+  },
+};
